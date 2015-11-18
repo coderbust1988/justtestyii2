8df3b7aff2d09f4e4c47fcc9bf85b21ee5d2e1dc
yanggang <yanggang@medlinker.net>
2015-11-18 08:14:49 +0800

    1

--- index.php
+++ index.php
@@ -2,49 +2,61 @@
 <html lang="en">
 <head>
 
-<script src="./src-min-noconflict/ace.js" type="text/javascript" charset="utf-8"></script>
-<script src="./src-min-noconflict/mode-javascript" type="text/javascript" charset="utf-8"></script>
-<script src="./src-min-noconflict/theme-twilight.js" type="text/javascript" charset="utf-8"></script>
-
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <title>Editor</title>
-  <style type="text/css" media="screen">
-    body {
-        overflow: hidden;
-    }
-
-    #editor {
-        margin: 0;
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-    }
+  <style type="text/css" media="screen">   
+#editor {
+    position: absolute;
+    top:  0px;
+    left: 80px;
+    bottom: 0px;
+    right: 0px;
+}
   </style>
+
+
 </head>
 <body>
 
-<div id="editor">function foo(items) {
-    var i;
-    for (i = 0; i &lt; items.length; i++) {
-        alert("Ace Rocks " + items[i]);
-    }
+	<div id="editor">function foo(items) {
+    var x = "All this is syntax highlighted";
+    return x;
 }</div>
+
+<script src="./src-min-noconflict/ace.js" type="text/javascript" charset="utf-8"></script>
+
+<script src="./src-min-noconflict/mode-javascript.js" type="text/javascript" charset="utf-8"></script>
+
+<script src="./src-min-noconflict/mode-java.js" type="text/javascript" charset="utf-8"></script>
+
+
+<script src="./src-min-noconflict/theme-eclipse.js" type="text/javascript" charset="utf-8"></script>
+
+<script>
+    var editor = ace.edit("editor");
+    //editor.setTheme("ace/theme/eclipse");
+    editor.getSession().setMode("ace/mode/java");
+</script>
 
 
 
 <script>
 
     var editor = ace.edit("editor");
-    alert(editor);
+    //alert(editor);
     editor.setTheme("ace/theme/twilight");
     var JavaScriptMode = ace.require("ace/mode/javascript").Mode;
     editor.session.setMode(new JavaScriptMode());
 
 
+function btn1Fuc(){
+	alert(editor.getValue());
+}
+
 </script>
+
+<button id="btn1" onclick="btn1Fuc()">btn1</button>
 
 </body>
 </html>
